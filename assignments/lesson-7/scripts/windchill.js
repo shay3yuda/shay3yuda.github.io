13e4192def7f3c6219701fc21c0ad38a038facb6
shay3yuda <contact.shay3@gmail.com>
2018-02-23 21:02:52 -0700

    added windchill javascript

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,24 +1,30 @@
-function windchill(){
-    
-    var t = windspeed();
-    var s = averageTemp();
-    
-    var f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16; 
-    
-    document.getElementById("windChill").innerHTML = f;
-}
+    var wch = windChill(); 
 
-
-// WINDSPEED FUNCTION
-
-function windspedd(){
+function windChill() {  
+    var high = 90
     
+    var low = 60
     
+    var s = 5
+    
+    var t = averageTemp(high, low);
+
+    var sPow = Math.pow(s, .16);
+    
+    var f = 35.74 + (0.6215 * t) + (-35.75 * sPow) + (0.4275 * t * sPow);
+
+    frounded =  f.toFixed(2);
+    return frounded;
 }
 
 // AVERAGE TEMPATER FUNCTION    
 
-function averageTemp() {
+function averageTemp(high, low) {
     
+    var h = high
+    var l = low
     
+    var average = (h + l) / 2
+    return average;
 }
+document.getElementById("windchill").innerHTML = wch + " &#8457;";
