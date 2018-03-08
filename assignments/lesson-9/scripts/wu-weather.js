75c628997adf23755e259071145cf0373e8de193
shay3yuda <35238509+shay3yuda@users.noreply.github.com>
2018-03-07 17:47:59 -0700

    update

--- assignments/lesson-9/scripts/wu-weather.js
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -1,25 +1,43 @@
-var hTemp = document.querySelector("#highTemp");
-var lTemp = document.querySelector("#lowTemp");
-var currentTemp = document.querySelector ("#currentTemp");
-var written = document.querySelector("#writtenWeather");
-var windspeed = document.querySelector("#windspeed");
-var icon = document.querySelector("icon");
 
-var requestURL = 'https://api.wunderground.com/api/45c5dafb23a1ef08/forecast/q/MN/Franklin.json';
 
-var request = new XMLHttpRequest();
-request.open("GET", requestURL);
+var weatherObject = new XMLHttpRequest();
+weatherObject.open("GET", 'https://api.wunderground.com/api/45c5dafb23a1ef08/conditions/q/MN/Franklin.json', true);
 
-request.responseType = "json";
-request.send();
+weatherObject.send();
 
-request.onload = function () {
+weatherObject.onload = function() {
     
-    var currentCon = request.response;
-    currenttemp(currentCon);
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById("currentTemp").innerHTML = weatherInfo.current_observation.temp_f + " &deg;F";
+    
+    document.getElementById("windspeed").innerHTML = weatherInfo.current_observation.wind_gust_mph + " MPH";
+    
+    document.getElementById("icon").src = weatherInfo.current_observation.icon_url;
+    
+    document.getElementById("windchill").innerHTML = weatherInfo.current_observation.windchill_f + " &deg;F";
+    
+    document.getElementById("precipitation").innerHTML = weatherInfo.current_observation.precip_today_in + " Inches Today";
 }
 
-function currenttemp(jsonObj) {
-    
-    var mytemp = jsonobj[]
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
