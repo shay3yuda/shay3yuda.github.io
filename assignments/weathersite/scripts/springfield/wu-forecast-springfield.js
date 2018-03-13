e1192c17448919db83efc8f5ce7747ee5d1bd30b
shay3yuda <35238509+shay3yuda@users.noreply.github.com>
2018-03-13 06:44:27 -0600

    updates and added to assigment page

---
+++ assignments/weathersite/scripts/springfield/wu-forecast-springfield.js
@@ -0,0 +1,16 @@
+
+var forecastObject = new XMLHttpRequest();
+forecastObject.open("GET", 'https://api.wunderground.com/api/45c5dafb23a1ef08/forecast/q/MN/Springfield.json', true);
+
+forecastObject.send();
+
+forecastObject.onload = function() {
+    
+    var forecastInfo = JSON.parse(forecastObject.responseText);
+    console.log(forecastInfo);
+    
+    document.getElementById("writtenWeather").innerHTML = forecastInfo.forecast.txt_forecast.forecastday["0"].fcttext;
+    
+    
+
+}
