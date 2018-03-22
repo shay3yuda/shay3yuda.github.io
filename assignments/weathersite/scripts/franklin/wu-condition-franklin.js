70cd5fdf4db8ee191bbce54aa2ced98cd6c149f0
shay3yuda <contact.shay3@gmail.com>
2018-03-21 20:06:18 -0600

    blaa

--- assignments/weathersite/scripts/franklin/wu-condition-franklin.js
+++ assignments/weathersite/scripts/franklin/wu-condition-franklin.js
@@ -1,51 +1,46 @@
-
-
 var weatherObject = new XMLHttpRequest();
-weatherObject.open("GET", 'https://api.wunderground.com/api/45c5dafb23a1ef08/conditions/q/MN/Franklin.json', true);
-
+weatherObject.open('GET', '//api.wunderground.com/api/4326409d402b76d5/conditions/q/MN/Franklin.json' , true);
 weatherObject.send();
 
 weatherObject.onload = function() {
-    
-   var weatherInfo = JSON.parse(weatherObject.responseText);
+    var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
     
-    document.getElementById('weatherCondition').innerHTML = weatherInfo.current_observation.weather;
-    
-    document.getElementById('precipitation').innerHTML = weatherInfo.current_observation.precip_today_in + " Inches Today";
-    
-    document.getElementById("windchill").innerHTML = weatherInfo.current_observation.windchill_f + " &deg;F";
-    
     document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
+    document.getElementById('precipitation').innerHTML = weatherInfo.current_observation.precip_1hr_in;
+    document.getElementById('windchill').innerHTML = weatherInfo.current_observation.windchill_f;
+    document.getElementById('windspeed').innerHTML = weatherInfo.current_observation.wind_mph;
     
     var icon_path = weatherInfo.current_observation.icon_url;
-    
     var urlString = document.location.href;
     console.log(urlString);
     var found = urlString.indexOf("https");
     console.log(found);
     
-    if(found>=0) {
-        icon_path = icon_path.replace("http","https");
+    if(found>=0){
+        icon_path = icon_path.replace("http", "https");
     }
     
-    document.getElementById('icon').src = icon_path
+    document.getElementById('icon').src = icon_path;
     
-    document.getElementById('windspeed').innerHTML = weatherInfo.current_observation.wind_mph;
-    
-    document.getElementById('writtenWeather').innerHTML = weatherInfo.forecast.txt_forecast.forecastday["0"].fcttext;
 }
 
+var forcastObject = new XMLHttpRequest();
+forcastObject.open('GET', '//api.wunderground.com/api/4326409d402b76d5/forecast/q/MN/Franklin.json', true);
+forcastObject.send();
 
-
-
-
-
-
-
-
-
-
+forcastObject.onload = function(){
+    var forcastInfo= JSON.parse(forcastObject.responseText);
+    console.log(forcastInfo);
+    
+    /* document.getElementById('hi').innerHTML = forcastInfo.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
+    document.getElementById('low').innerHTML = forcastInfo.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
+    document.getElementById('conditions').innerHTML = forcastInfo.forecast.simpleforecast.forecastday["0"].conditions;
+    */
+    
+    document.getElementById('forcast').innerHTML = forcastInfo.forecast.txt_forecast.forecastday["0"].fcttext;
+    
+}
 
 
 
