f2f5c8c098196051e7ab0dc47ce5eb233d7ecc18
shay3yuda <contact.shay3@gmail.com>
2018-03-21 20:17:16 -0600

    now greenville data

--- assignments/weathersite/scripts/greenville/wu-condition-greenville.js
+++ assignments/weathersite/scripts/greenville/wu-condition-greenville.js
@@ -10,32 +10,38 @@ weatherObject.onload = function() {
     var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
     
-    document.getElementById("currentTemp").innerHTML = weatherInfo.current_observation.temp_f + " &deg;F";
+    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
+    document.getElementById('precipitation').innerHTML = weatherInfo.current_observation.precip_1hr_in;
+    document.getElementById('windchill').innerHTML = weatherInfo.current_observation.windchill_f;
+    document.getElementById('windspeed').innerHTML = weatherInfo.current_observation.wind_mph;
     
-    document.getElementById("windspeed").innerHTML = weatherInfo.current_observation.wind_gust_mph + " MPH";
+    var icon_path = weatherInfo.current_observation.icon_url;
+    var urlString = document.location.href;
+    console.log(urlString);
+    var found = urlString.indexOf("https");
+    console.log(found);
     
-    document.getElementById("windchill").innerHTML = weatherInfo.current_observation.windchill_f + " &deg;F";
+    if(found>=0){
+        icon_path = icon_path.replace("http", "https");
+    }
     
-    document.getElementById("precipitation").innerHTML = weatherInfo.current_observation.precip_today_in + " Inches Today";
+    document.getElementById('icon').src = icon_path;
+    
 }
 
+var forcastObject = new XMLHttpRequest();
+forcastObject.open('GET', '//api.wunderground.com/api/4326409d402b76d5/forecast/q/TX/Greenville.json', true);
+forcastObject.send();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+forcastObject.onload = function(){
+    var forcastInfo= JSON.parse(forcastObject.responseText);
+    console.log(forcastInfo);
+    
+    /* document.getElementById('hi').innerHTML = forcastInfo.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
+    document.getElementById('low').innerHTML = forcastInfo.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
+    document.getElementById('conditions').innerHTML = forcastInfo.forecast.simpleforecast.forecastday["0"].conditions;
+    */
+    
+    document.getElementById('forcast').innerHTML = forcastInfo.forecast.txt_forecast.forecastday["0"].fcttext;
+    
+}
