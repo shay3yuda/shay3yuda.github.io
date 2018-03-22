47496894e34790ac30237fea68df470ca837bd2c
shay3yuda <contact.shay3@gmail.com>
2018-03-21 19:39:34 -0600

    icon update

--- assignments/weathersite/scripts/franklin/wu-condition-franklin.js
+++ assignments/weathersite/scripts/franklin/wu-condition-franklin.js
@@ -7,18 +7,34 @@ weatherObject.send();
 
 weatherObject.onload = function() {
     
-    var weatherInfo = JSON.parse(weatherObject.responseText);
+   var franklinWeather = JSON.parse(request.responseText);
     console.log(weatherInfo);
     
-    document.getElementById("currentTemp").innerHTML = weatherInfo.current_observation.temp_f + " &deg;F";
+    document.getElementById('weatherCondition').innerHTML = weatherInfo.current_observation.weather;
     
-    document.getElementById("windspeed").innerHTML = weatherInfo.current_observation.wind_gust_mph + " MPH";
+    document.getElementById('precipitation').innerHTML = weatherInfo.current_observation.precip_today_in + " Inches Today";
     
     document.getElementById("windchill").innerHTML = weatherInfo.current_observation.windchill_f + " &deg;F";
     
-    document.getElementById("precipitation").innerHTML = weatherInfo.current_observation.precip_today_in + " Inches Today";
+    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
+    
+    var icon_path = weatherInfo.current_observation.icon_url;
+    
+    var urlString = document.location.href;
+    console.log(urlString);
+    var found = urlString.indexOf("https");
+    console.log(found);
+    
+    if(found>=0) {
+        icon_path = icon_path.replace("http","https");
+    }
+    
+    document.getElementById('icon').src = icon_path
+    
+    document.getElementById('windspeed').innerHTML = weatherInfo.current_observation.wind_mph;
+    
+    document.getElementById('writtenWeather').innerHTML = weatherInfo.forecast.txt_forecast.forecastday["0"].fcttext;
 }
-
 
 
 
