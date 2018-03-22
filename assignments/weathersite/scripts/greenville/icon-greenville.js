3092238383ef0d3a306a1b01829d937f15bb2a5a
shay3yuda <contact.shay3@gmail.com>
2018-03-21 19:27:22 -0600

    Icon fix

--- assignments/weathersite/scripts/greenville/icon-greenville.js
+++ assignments/weathersite/scripts/greenville/icon-greenville.js
@@ -1,19 +1,34 @@
-var weatherObject = new XMLHttpRequest();
-weatherObject.open("GET", 'https://api.wunderground.com/api/45c5dafb23a1ef08/conditions/q/TX/Greenville.json', true);
+var requestURL = '//api.wunderground.com/api/997fa104ce7cd04a/conditions/forecast/q/MN/Franklin.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL, true);
+request.send();
+
+request.onload = function() {
+    var franklinWeather = JSON.parse(request.responseText);
+    console.log(weatherInfo);
     
-weatherObject.send();
-
-weatherObject.onload = function() {
-
-var icon_path = weatherInfo.current_observation.icon_url;
+    document.getElementById('weatherCondition').innerHTML = weatherInfo.current_observation.weather;
+    
+    document.getElementById('precipitation').innerHTML = weatherInfo.current_observation.precip_today_in + " Inches Today";
+    
+    document.getElementById("windchill").innerHTML = weatherInfo.current_observation.windchill_f + " &deg;F";
+    
+    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
+    
+    var icon_path = weatherInfo.current_observation.icon_url;
+    
     var urlString = document.location.href;
     console.log(urlString);
     var found = urlString.indexOf("https");
     console.log(found);
     
-    if(found>=0){
-        icon_path = icon_path.replace("http", "https");
+    if(found>=0) {
+        icon_path = icon_path.replace("http","https");
     }
     
-    document.getElementById('w_icon').src = icon_path;
+    document.getElementById('icon').src = icon_path
+    
+    document.getElementById('windspeed').innerHTML = weatherInfo.current_observation.wind_mph;
+    
+    document.getElementById('writtenWeather').innerHTML = weatherInfo.forecast.txt_forecast.forecastday["0"].fcttext;
 }
